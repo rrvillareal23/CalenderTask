c6e4a776b437e377c297b213e9272163ed31fe5c
Robert (Ricky) Villareal <112342959+rrvillareal23@users.noreply.github.com>
2024-06-09 07:47:19 -0700

    Initial Commit

---
+++ FamilyApp/FamilyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyAppApp.swift
+//  FamilyApp
+//
+//  Created by Ricky Villareal on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FamilyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
